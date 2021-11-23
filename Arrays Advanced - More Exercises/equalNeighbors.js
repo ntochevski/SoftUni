a9fba0fb8eaf3dd7181bb1ec8b73e3318d85d4f5
ntochevski <tochevski.nikolay@gmail.com>
2021-11-23 16:52:53 +0200

    Fixed issues

--- Arrays Advanced - More Exercises/equalNeighbors.js
+++ Arrays Advanced - More Exercises/equalNeighbors.js
@@ -1,17 +1,47 @@
 function solve(arr) {
-    let numberOfEqualPairs = 0;
-   for (let i = 0; i < arr.length - 1; i++) {
-       for (let element of arr[i]) {
-           
-           if (arr[i + 1].includes(element)) {
-               let currentIndex = arr[i].indexOf(element);
-               let nextIndex = arr[i + 1].indexOf(element);
-               if (currentIndex === nextIndex) {
-                   numberOfEqualPairs++;
-               }
-           }
-       }
-       
+    let fullMatch = 0;
+
+    verticalMatch(arr);
+    for (let line of arr) {
+        horyzontalMatch(line);
     }
-    return numberOfEqualPairs;
+
+    function horyzontalMatch(input) {
+        for (const el of input) {
+            let current = input.shift()
+            for (const element of input) {
+                if (element === current) {
+                    fullMatch++;
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+
+    }
+    function verticalMatch (input) {
+       
+        for (let i = 0; i < arr.length - 1; i++) {
+            let current = arr[i];
+            for (let j = 0; j < current.length; j++) {
+                let currentDigit = current[j];
+              
+                let nextDigit = arr[i + 1][j];
+
+                if (currentDigit === nextDigit) {
+                    fullMatch++
+                }
+                
+            }
+            
+            
+        }
+    }
+    console.log(fullMatch);
 }
+solve([['2', '2', '5', '7', '4'], 
+
+['4', '0', '5', '3', '4'], 
+
+['2', '5', '5', '4', '2']] );
